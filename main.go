6556d54afb2aed9d0c5b459b260c63eb276351ec
sgurucharan2015-ship-it <sgurucharan.2015@gmail.com>
2026-04-19 19:47:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/websocket"
 )
+
+type Player struct {
+	ID string  `json:"id"`
+	X  float64 `json:"x"`
+	Z  float64 `json:"z"`
+}
+
+var players = make(map[string]Player)
+var clients = make(map[*websocket.Conn]string)
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool { return true },
@@ -15,17 +25,38 @@ var upgrader = websocket.Upgrader{
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("Upgrade error:", err)
 		return
 	}
-	defer conn.Close()
+
+	id := conn.RemoteAddr().String()
+	clients[conn] = id
+
+	log.Println("Player connected:", id)
 
 	for {
-		mt, msg, err := conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
+			delete(clients, conn)
+			delete(players, id)
+			conn.Close()
 			break
 		}
-		conn.WriteMessage(mt, msg)
+
+		var p Player
+		json.Unmarshal(msg, &p)
+
+		p.ID = id
+		players[id] = p
+
+		broadcast()
+	}
+}
+
+func broadcast() {
+	data, _ := json.Marshal(players)
+
+	for conn := range clients {
+		conn.WriteMessage(websocket.TextMessage, data)
 	}
 }
 
@@ -37,6 +68,6 @@ func main() {
 
 	http.HandleFunc("/ws", wsHandler)
 
-	fmt.Println("Server running on port", port)
+	log.Println("Server running on port", port)
 	http.ListenAndServe(":"+port, nil)
 }
